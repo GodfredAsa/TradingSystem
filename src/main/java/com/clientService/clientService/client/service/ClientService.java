ef1c1f66518451487028f6c223c6d603145c97a9
root <root@DESKTOP-M2D1LCR.localdomain>
2021-11-25 16:10:06 +0000

    created client model

---
+++ src/main/java/com/clientService/clientService/client/service/ClientService.java
@@ -0,0 +1,38 @@
+package com.clientService.clientService.client.service;
+
+import com.clientService.clientService.client.model.Client;
+import com.clientService.clientService.client.loggerPack.ClientLogger;
+import com.clientService.clientService.client.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ClientService {
+    private ClientRepository clientRepository;
+    private ClientLogger clientLogger;
+//    private SecurityConfiguration securityConfig;
+
+    public String addClient(Client client) {
+        try {
+//            String encodedPassword = securityConfig.passwordEncoder().encode(client.getPassword());
+//            client.setPassword(encodedPassword);
+                clientRepository.save(client);
+                clientLogger.LOGGER.info("Client with id: " + client.getId() + " created successfully");
+                return "Client added successfully";
+
+        } catch (Exception e) {
+            clientLogger.LOGGER.error(e.getMessage());
+            return "Client could not be added, try again";
+        }
+
+    }
+
+    public Optional<Client> getClient(Long id){
+        clientLogger.LOGGER.info("Client with id:" +id + " accessed from the database");
+        return clientRepository.findById(id);
+    }
+}
