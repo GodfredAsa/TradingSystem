80c4372a144b556a53a6765745792fa5ca47d26e
Abdul Faheem Kassim <kassimalfaheem@gmail.com>
2021-12-05 17:12:37 +0000

    Added Echange2 ChannelTopic and Listeners

--- src/main/java/com/example/demo/configuration/RedisConfiguration.java
+++ src/main/java/com/example/demo/configuration/RedisConfiguration.java
@@ -1,17 +1,17 @@
 package com.example.demo.configuration;
 
-import com.example.demo.entities.Product;
-import com.example.demo.reportLoggerService.Subscriber;
+import com.example.demo.reportLoggerService.ExchangeSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @Configuration
 public class RedisConfiguration {
@@ -21,8 +21,11 @@ public class RedisConfiguration {
     @Value("${redis.port}")
     private int redisPort;
 
-    @Value("${redis.marketdatachannel}")
-    private String redisMarketDataChannel;
+    @Value("${redis.exchange1channel}")
+    private String exchange1Channel;
+
+    @Value("${redis.exchange2channel}")
+    private String exchange2Channel;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
@@ -34,19 +37,22 @@ public class RedisConfiguration {
 
     @Bean
     public ChannelTopic channelTopic() {
-        return new ChannelTopic(redisMarketDataChannel);
+        return new ChannelTopic(exchange1Channel);
     }
 
     @Bean
+    public ChannelTopic channelTopic2() {return new ChannelTopic(exchange2Channel);}
+
+    @Bean
     public MessageListenerAdapter messageListenerAdapter() {
-        return new MessageListenerAdapter(new Subscriber());
+        return new MessageListenerAdapter(new ExchangeSubscriber());
     }
 
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer() {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(jedisConnectionFactory());;
-        container.addMessageListener(messageListenerAdapter(),channelTopic());
+        container.setMessageListeners(Map.of(messageListenerAdapter(), Arrays.asList(channelTopic(),channelTopic2())));
         return container;
     }
 }
---
+++ src/main/java/com/example/demo/reportLoggerService/ExchangeSubscriber.java
@@ -0,0 +1,38 @@
+package com.example.demo.reportLoggerService;
+
+
+import com.example.demo.entities.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+
+public class ExchangeSubscriber implements MessageListener {
+    Logger logger =  LoggerFactory.getLogger(ExchangeSubscriber.class);
+
+    @SneakyThrows
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        if (new String(message.getChannel(), StandardCharsets.UTF_8).equals("exchange1")){
+            ObjectMapper objectMapper = new ObjectMapper();
+            String body = new String(message.getBody());
+
+            Product[] msg = objectMapper.readValue(body, Product[].class);
+            logger.info("Consumed Message1 {}", Arrays.asList(msg));
+
+        } else if(new String(message.getChannel(), StandardCharsets.UTF_8).equals("exchange2")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String body = new String(message.getBody());
+
+            Product[] msg = objectMapper.readValue(body, Product[].class);
+            logger.info("Consumed Message2 {}", Arrays.asList(msg));
+        }
+    }
+}
