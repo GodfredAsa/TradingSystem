072cefeeec411b5af0f6d79ed6dee3d1849fea27
Abdul Faheem Kassim <38706000+skiddow123@users.noreply.github.com>
2021-12-09 15:50:50 +0000

    Added system.properties file

--- src/main/java/com/example/marketdataservice/controllers/RedisMessagePublisher.java
+++ src/main/java/com/example/marketdataservice/controllers/RedisMessagePublisher.java
@@ -1,22 +1,22 @@
 package com.example.marketdataservice.controllers;
 
 import com.example.marketdataservice.models.Product;
-import com.example.marketdataservice.services.MarketDataService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/marketDataService")
 public class RedisMessagePublisher {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Product> redisTemplate;
 
     @Autowired
     private ChannelTopic channelTopic;
@@ -25,7 +25,7 @@ public class RedisMessagePublisher {
     private ChannelTopic channelTopic2;
 
     @ApiOperation("Subscription to Exchange1 Data")
-    @PostMapping("/publish") //TODO: Subscribe to Exchange1 with URL
+    @PostMapping("/g6webhookExchange1URL") //TODO: Subscribe to Exchange1 with URL
     public String publish(@RequestBody List<Product> message) {
         redisTemplate.convertAndSend(channelTopic.getTopic(), message);
         return "Exchange1 Data Published";
@@ -33,7 +33,7 @@ public class RedisMessagePublisher {
 
 
     @ApiOperation("Subscription to Exchange2 Data")
-    @PostMapping("/publish2") //TODO: Subscribe to Exchange2 with URL
+    @PostMapping("/g6webhookExchange2URL") //TODO: Subscribe to Exchange2 with URL
     public String publish2(@RequestBody List<Product> message) {
         redisTemplate.convertAndSend(channelTopic2.getTopic(), message);
         return "Exchange2 Data Published";
