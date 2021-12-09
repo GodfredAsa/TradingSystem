da20f9f640091e4a32569d52d0f627fdacdf1372
Abdul Faheem Kassim <38706000+skiddow123@users.noreply.github.com>
2021-12-09 17:08:44 +0000

    changed lastTradedPrice from String to double in Product.java

--- src/main/java/com/example/demo/configuration/ActiveMQConfiguration.java
+++ src/main/java/com/example/demo/configuration/ActiveMQConfiguration.java
@@ -1,22 +1,29 @@
 package com.example.demo.configuration;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableJms
 public class ActiveMQConfiguration {
     private String DEFAULT_BROKER_URL = "tcp://localhost:61616";
-    private String MESSAGE_QUEUE = "authentication-log-queue";
 
+    @Bean
     public ConnectionFactory connectionFactory(){
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(DEFAULT_BROKER_URL);
         activeMQConnectionFactory.setTrustedPackages(List.of("com.example"));
+        activeMQConnectionFactory.setUserName("admin");
+        activeMQConnectionFactory.setPassword("admin");
         return activeMQConnectionFactory;
     }
+
 
 }
--- src/main/java/com/example/demo/reportLoggerController/QueueMessageListener.java
+++ src/main/java/com/example/demo/reportLoggerController/QueueMessageListener.java
@@ -3,10 +3,12 @@ package com.example.demo.reportLoggerController;
 import com.example.demo.entities.AuthenticationLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
 
 public class QueueMessageListener {
     Logger logger = LoggerFactory.getLogger(QueueMessageListener.class);
 
+    @JmsListener(destination = "authentication_log_queue")
     public void logUserAuthentication(AuthenticationLog authenticationLog){
         logger.info("User Authentication Log: {}", authenticationLog);
     }
