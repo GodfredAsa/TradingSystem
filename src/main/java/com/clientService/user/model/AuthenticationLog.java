fb95c533b3e6368adc09f45615f3a0ab2c55734d
Abdul Faheem Kassim <38706000+skiddow123@users.noreply.github.com>
2021-12-13 19:37:40 +0000

    Completed Deserialization
    Completed Deserialization

--- src/main/java/com/clientService/user/model/AuthenticationLog.java
+++ src/main/java/com/clientService/user/model/AuthenticationLog.java
@@ -2,6 +2,10 @@ package com.clientService.user.model;
 
 import com.clientService.enums.AuthStatus;
 import com.clientService.enums.UserRole;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,9 +14,12 @@ import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Data
+@JsonSerialize
 public class AuthenticationLog implements Serializable {
     private Long userID;
     private AuthStatus authStatus;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime localDateTime;
     private UserRole role;
 }
