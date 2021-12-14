6ecbce32a1bd17243dd3dbb0e6252afb50604982
Andivi <43036764+Andivi@users.noreply.github.com>
2021-12-14 18:44:47 +0000

    Status Update

--- src/main/java/com/example/demo/enums/Status.java
+++ src/main/java/com/example/demo/enums/Status.java
@@ -2,5 +2,10 @@ package com.example.demo.enums;
 
 //Order Status
 public enum Status {
-    OPENED, CLOSED
+    PARTIALLY_PURCHASED,
+    PARTIALLY_SOLD,
+    SOLD,
+    PURCHASED,
+    REJECTED,
+    PENDING
 }
