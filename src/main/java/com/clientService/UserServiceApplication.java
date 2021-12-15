19458ccbdbf07ad2964041d5eabc211cb6a3232e
attakay78 <richardquaicoe78@gmail.com>
2021-12-15 05:31:06 +0000

    change to application.yml file

--- src/main/java/com/clientService/UserServiceApplication.java
+++ src/main/java/com/clientService/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
